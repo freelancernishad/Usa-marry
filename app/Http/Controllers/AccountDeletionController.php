ef3d775264be3eb13622a2485b47106ba9ffeee4
freelancernishad <freelancernishad123@gmail.com>
2025-10-09 17:22:21 +0600

    update

--- app/Http/Controllers/AccountDeletionController.php
+++ app/Http/Controllers/AccountDeletionController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
 use App\Mail\AccountDeletionMail;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 
 class AccountDeletionController extends Controller
 {
     public function deleteRequest(Request $request)
     {
+        Log::info('Received method: ' . $request->method());
         $validated = $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|email',

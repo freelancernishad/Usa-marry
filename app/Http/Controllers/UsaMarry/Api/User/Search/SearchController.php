b67c58ff7461c02328ba6730fbe81d8b3db3e575
freelancernishad <freelancernishad123@gmail.com>
2025-10-26 21:33:22 +0600

    update

--- app/Http/Controllers/UsaMarry/Api/User/Search/SearchController.php
+++ app/Http/Controllers/UsaMarry/Api/User/Search/SearchController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\UserPaginationResource;
 
 class SearchController extends Controller
 {

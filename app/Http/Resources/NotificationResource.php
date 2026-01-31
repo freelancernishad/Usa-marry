ce856b5af61b15193d1835f0341917f66a2a7acf
freelancernishad <freelancernishad123@gmail.com>
2026-01-31 18:02:34 +0600

    update

--- app/Http/Resources/NotificationResource.php
+++ app/Http/Resources/NotificationResource.php
@@ -10,11 +10,16 @@ class NotificationResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
+           'id' => $this->id,
+            'user_id' => $this->user_id,
+            'admin_id' => $this->admin_id,
             'type' => $this->type,
             'message' => $this->message,
+            'related_model' => $this->related_model,
+            'related_model_id' => $this->related_model_id,
             'is_read' => $this->is_read,
             'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
 
             'user' => $this->whenLoaded('user', function () {
                 return [
